c744c46a0f786da0b127e3a31d6de84f56dc7251
seizethedaylyn <rlacofls294@gmail.com>
2023-11-30 18:03:04 +0900

    Update BoardApplication

--- src/main/java/com/rlacofls/board/BoardApplication.java
+++ src/main/java/com/rlacofls/board/BoardApplication.java
@@ -1,28 +1,28 @@
-//package com.rlacofls.board;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
-//public class BoardApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(BoardApplication.class, args);
-//	}
-//
-//	@Bean
-//	public WebMvcConfigurer corsCongigurer(){
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry){
-//				registry.addMapping("/**").allowedOriginPatterns();
-//			}
-//		};
-//	}
-//	//controller 마다 작성하기 귀찮으니까 앞에 미리 설정
-//
-//}
+package com.rlacofls.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsCongigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedOriginPatterns();
+			}
+		};
+	}
+	//controller 마다 작성하기 귀찮으니까 앞에 미리 설정
+
+}
