a22a9007e09d8f9f46e537bfafaf123b861dfa85
seizethedaylyn <rlacofls294@gmail.com>
2023-11-30 17:50:27 +0900

    Update CommentEntity.java

--- src/main/java/com/rlacofls/board/entity/CommentEntity.java
+++ src/main/java/com/rlacofls/board/entity/CommentEntity.java
@@ -1,23 +1,23 @@
-//package com.rlacofls.board.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity(name="Comment")
-//@Table(name="Comment")
-//public class CommentEntity {
-//    @Id
-//    @GeneratedValue(strategy= GenerationType.IDENTITY)
-//    private int commentId;
-//    private int boardNumber;
-//    private String userEmail;
-//    private String commentUserProfile;
-//    private String commentUserNickname;
-//    private String commentWriteDate;
-//    private String commentContent;
-//}
+package com.rlacofls.board.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="Comment")
+@Table(name="Comment")
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int commentId;
+    private int boardNumber;
+    private String userEmail;
+    private String commentUserProfile;
+    private String commentUserNickname;
+    private String commentWriteDate;
+    private String commentContent;
+}
