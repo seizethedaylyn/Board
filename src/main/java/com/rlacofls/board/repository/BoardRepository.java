35c2f6f50d6484f1e0a9f28c7648c6d2d2ac550d
seizethedaylyn <rlacofls294@gmail.com>
2023-11-30 17:54:30 +0900

    Update BoardRepository

--- src/main/java/com/rlacofls/board/repository/BoardRepository.java
+++ src/main/java/com/rlacofls/board/repository/BoardRepository.java
@@ -1,9 +1,9 @@
-//package com.rlacofls.board.repository;
-//
-//import com.rlacofls.board.entity.BoardEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
-//}
+package com.rlacofls.board.repository;
+
+import com.rlacofls.board.entity.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
+}
