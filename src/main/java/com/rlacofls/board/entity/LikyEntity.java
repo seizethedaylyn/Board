ab1ff9dff37c45a23ce71dcd4a92af06406d2300
seizethedaylyn <rlacofls294@gmail.com>
2023-11-30 17:50:13 +0900

    Update LikyEntity.java

--- src/main/java/com/rlacofls/board/entity/LikyEntity.java
+++ src/main/java/com/rlacofls/board/entity/LikyEntity.java
@@ -1,21 +1,21 @@
-//package com.rlacofls.board.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity(name="Liky")
-//@Table(name="Liky")
-//public class LikyEntity {
-//    @Id
-//    @GeneratedValue(strategy= GenerationType.IDENTITY)
-//    private int likeId;
-//    private int boardNumber;
-//    private String userEmail;
-//    private String likeUserProfile;
-//    private String likeUserNickname;
-//}
+package com.rlacofls.board.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="Liky")
+@Table(name="Liky")
+public class LikyEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int likeId;
+    private int boardNumber;
+    private String userEmail;
+    private String likeUserProfile;
+    private String likeUserNickname;
+}
