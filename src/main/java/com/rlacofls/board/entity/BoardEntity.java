79df51b295573e482c557622d30e38d1f71f2bc9
seizethedaylyn <rlacofls294@gmail.com>
2023-11-30 17:50:39 +0900

    Update BoardEntity.java

--- src/main/java/com/rlacofls/board/entity/BoardEntity.java
+++ src/main/java/com/rlacofls/board/entity/BoardEntity.java
@@ -1,29 +1,29 @@
-//package com.rlacofls.board.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Data
-//@Entity(name="Board")
-//@Table(name="Board")
-//public class BoardEntity {
-//    @Id
-//    @GeneratedValue(strategy= GenerationType.IDENTITY) // id 자동 생성
-//    private int boardNumber;
-//    private String boardTitle;
-//    private String boardContent;
-//    private String boardImage;
-//    private String boardVideo;
-//    private String boardFile;
-//    private String boardWriterEmail;
-//    private String boardWriterProfile;
-//    private String boardWriterNickname;
-//    private String boardWriteDate;
-//    private int boardClickCount;
-//    private int boardLikeCount;
-//    private int boardCommentCount;
-//}
+package com.rlacofls.board.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name="Board")
+@Table(name="Board")
+public class BoardEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY) // id 자동 생성
+    private int boardNumber;
+    private String boardTitle;
+    private String boardContent;
+    private String boardImage;
+    private String boardVideo;
+    private String boardFile;
+    private String boardWriterEmail;
+    private String boardWriterProfile;
+    private String boardWriterNickname;
+    private String boardWriteDate;
+    private int boardClickCount;
+    private int boardLikeCount;
+    private int boardCommentCount;
+}
