9836109e4ec4ad15f90f39aada4a889ecc8eb18b
seizethedaylyn <rlacofls294@gmail.com>
2023-11-30 17:53:53 +0900

    Update PopularSearchRepository

--- src/main/java/com/rlacofls/board/repository/PopularSearchRepository.java
+++ src/main/java/com/rlacofls/board/repository/PopularSearchRepository.java
@@ -1,9 +1,9 @@
-//package com.rlacofls.board.repository;
-//
-//import com.rlacofls.board.entity.PopularSearchEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface PopularSearchRepository extends JpaRepository<PopularSearchEntity, String> {
-//}
+package com.rlacofls.board.repository;
+
+import com.rlacofls.board.entity.PopularSearchEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PopularSearchRepository extends JpaRepository<PopularSearchEntity, String> {
+}
