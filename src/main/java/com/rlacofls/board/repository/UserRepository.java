2b7fa5ddfa2192cddc37d1e096472bb607631ec4
seizethedaylyn <rlacofls294@gmail.com>
2023-11-30 17:53:33 +0900

    Update UserRepository

--- src/main/java/com/rlacofls/board/repository/UserRepository.java
+++ src/main/java/com/rlacofls/board/repository/UserRepository.java
@@ -1,11 +1,11 @@
-//package com.rlacofls.board.repository;
-//
-//import com.rlacofls.board.entity.UserEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<UserEntity, String> {
-//    public boolean existsByUserEmailAndUserPassword(String userEmail, String userPassword);
-//}
-////디비와 서버간의 접점
+package com.rlacofls.board.repository;
+
+import com.rlacofls.board.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    public boolean existsByUserEmailAndUserPassword(String userEmail, String userPassword);
+}
+//디비와 서버간의 접점
