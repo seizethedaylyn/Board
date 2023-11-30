92d46410440806b081426103a9d893c6670ea190
seizethedaylyn <rlacofls294@gmail.com>
2023-11-30 17:54:08 +0900

    Update LikyRepository

--- src/main/java/com/rlacofls/board/repository/LikyRepository.java
+++ src/main/java/com/rlacofls/board/repository/LikyRepository.java
@@ -1,9 +1,9 @@
-//package com.rlacofls.board.repository;
-//
-//import com.rlacofls.board.entity.LikyEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface LikyRepository extends JpaRepository<LikyEntity, Integer> {
-//}
+package com.rlacofls.board.repository;
+
+import com.rlacofls.board.entity.LikyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikyRepository extends JpaRepository<LikyEntity, Integer> {
+}
